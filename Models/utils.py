05fb502853cf7b0caf8b4d083f035025d7c29b57
Aryan Raj <aryan.raj15604@gmail.com>
2023-02-19 01:18:49 +0530

    Fixed error in calculating the localYAxis

--- Models/utils.py
+++ Models/utils.py
@@ -14,7 +14,7 @@ def computeLocalYAxis(dirX: npt.NDArray[np.float64], beta: float) -> npt.NDArray
   """
   unrotated = np.array([-dirX[1], dirX[0], 0])
   K = computePreCrossProductTransform(dirX)
-  R: npt.NDArray[np.float64] = np.identity(3) + np.sin(beta/(2*np.pi))*K + (1 - np.cos(beta/(2*np.pi))) * K @ K
+  R: npt.NDArray[np.float64] = np.identity(3) + np.sin(beta/180.*np.pi)*K + (1 - np.cos(beta/180.*np.pi)) * K @ K
   return unrotated @ R.T
 
 def computePreCrossProductTransform(vec: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
